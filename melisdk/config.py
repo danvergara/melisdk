bf93fc331f807f98548413cce3919919fe259c99
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2019-07-01 22:37:29 -0500

    New config file

---
+++ melisdk/config.py
@@ -0,0 +1,18 @@
+CONFIG = {'sdk_version': 'MELI-PYTHON-SDK-2.0.0',
+          'api_root_url': 'https://api.mercadolibre.com',
+          'oauth_url': '/oauth/token',
+          'ssl_version': 'PROTOCOL_TLSv1'}
+
+AUTH_URL_DICT = {'ARG': 'https://auth.mercadolibre.com.ar',
+                 'BRA': 'https://auth.mercadolivre.com.br',
+                 'COL': 'https://auth.mercadolibre.com.co',
+                 'CRC': 'https://auth.mercadolibre.com.cr',
+                 'ECU': 'https://auth.mercadolibre.com.ec',
+                 'CHL': 'https://auth.mercadolibre.cl',
+                 'MXN': 'https://auth.mercadolibre.com.mx',
+                 'URY': 'https://auth.mercadolibre.com.uy',
+                 'VEN': 'https://auth.mercadolibre.com.ve',
+                 'PAN': 'https://auth.mercadolibre.com.pa',
+                 'PER': 'https://auth.mercadolibre.com.pe',
+                 'PRT': 'https://auth.mercadolibre.com.pt',
+                 'DMA': 'https://auth.mercadolibre.com.do'}

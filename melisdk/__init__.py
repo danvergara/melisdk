4f9f2ce8d5a45db3df0985936d8627952e770f44
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2019-07-16 12:55:14 -0500

    bumped to 0.1.3

--- melisdk/__init__.py
+++ melisdk/__init__.py
@@ -1,3 +1,3 @@
 from melisdk.meli import Meli
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='melisdk',
-    version="0.1.2",
+    version="0.1.3",
     description='A copy of MercadoLibre python SDK, but works',
     long_description=long_description,
     long_description_content_type="text/markdown",
